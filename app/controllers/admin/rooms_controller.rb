d4ceca747d4a243a99812c19eb9cd6c3b8e7a90b
hieufix1710 <hieufix1710@gmail.com>
2021-03-11 13:08:50 +0700

    room-new

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -1,29 +1,28 @@
 # frozen_string_literal: true
 
 class Admin::RoomsController < Admin::AdminController
-<<<<<<< HEAD
-    before_action :load_room_type, only: %i(new)
-  def new; end
+  before_action :load_room_types, only: %i(new create)
+
+  def new
+    @room=  Room.new
+  end
 
   def create
     @room = Room.create(room_params)
     if @room.save
       flash[:success] = t "success"
-      redirect_to admin_rooms_path
+      redirect_to admin_root_path
     else
-      flash.now[:warning] = t "sowething was wrong in form"
+      flash.now[:warning] = t "sowething_was_wrong_in_form"
       render :new
     end
   end
 
-=======
->>>>>>> admin-design
   def show
     @room_ids = Booking.room_ids_in_booking
     @temp = Room.rooms_booking @room_ids
     @rooms = @temp.paginate(page: params[:page]).per_page(Settings.paging_limit)
   end
-<<<<<<< HEAD
 
   def room_params
     params.require(:room).permit(:name,
@@ -32,9 +31,7 @@ class Admin::RoomsController < Admin::AdminController
 
   private
 
-  def load_room_type
-    @room_type = RoomType.all
+  def load_room_types
+    @room_types = RoomType.all
   end
-=======
->>>>>>> admin-design
 end
