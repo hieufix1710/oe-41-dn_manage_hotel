97fcccea5091c6f7716909bf41a55758886f1c2a
hieufix1710 <hieufix1710@gmail.com>
2021-03-17 10:07:05 +0700

    register user

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module HotelManager
         "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      "<div class=\"field_with_errors control-group error\">#{html_tag}</div>".html_safe}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root to: "static_pages#home"
     resource :rooms, only: :show
+    resource :users
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
     namespace :admin do
     end
   end
